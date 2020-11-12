4fb29a2865c167f9b32ca5417551b992528687b6
anpengwei <1553147738@qq.com>
2020-11-12 10:37:39 +0800

    手册地址

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,27 +1,37 @@
 function openFile(data) {
   let url;
-  (() => {
+  (function () {
     switch (data) {
       case 0:
-        return (url = "asset/PDF/汉语.pdf");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/23_zh.pdf");
       case 1:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/23_en.pdf");
       case 2:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/23_ko.pdf");
       case 3:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/23_ja.pdf");
       case 4:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/23_ru.pdf");
       case 5:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/29_zh.pdf");
       case 6:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/29_en.pdf");
       case 7:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/29_ko.pdf");
       case 8:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/29_ja.pdf");
       case 9:
-        return (url = "");
+        return (url =
+          "https://skyworth-cloud-image.oss-cn-beijing.aliyuncs.com/LGD2329/manual/29_ru.pdf");
     }
   })();
   const outFile = document.createElement("a");
